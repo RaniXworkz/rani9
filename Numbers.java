3746a36f520d67fe6cb3e7524816e4d1d6354c73
RaniXorkz <raniattigeri.xworkz@gmail.com>
2021-12-20 23:49:02 +0530

    hello nisarga

---
+++ Numbers.java
@@ -0,0 +1,17 @@
+class Numbers
+{
+public static void main (String[] a)
+{
+byte []ids={20,21,22,23,24,35,36,37,38,39};
+System.out.println(ids[0]);
+System.out.println(ids[1]);
+System.out.println(ids[2]);
+System.out.println(ids[3]);
+System.out.println(ids[4]);
+System.out.println(ids[5]);
+System.out.println(ids[6]);
+System.out.println(ids[7]);
+System.out.println(ids[8]);
+System.out.println(ids[9]);
+}
+}
